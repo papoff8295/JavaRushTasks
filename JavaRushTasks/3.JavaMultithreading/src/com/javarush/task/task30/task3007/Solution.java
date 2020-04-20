cf733fb7187fb5811b90fa7fb20a80aaa8eb1ab4
Vladimir Popov <papoff8295@gmail.com>
2020-04-20 18:43:07 +0300

    del

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3007/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3007/Solution.java
@@ -6,16 +6,25 @@ package com.javarush.task.task30.task3007;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(maxPowerOf2(140_000));   //131072
-        System.out.println(maxPowerOf2(1026));      //1024
-        System.out.println(maxPowerOf2(17));        //16
+       //System.out.println(maxPowerOf2(1026));      //1024
+       //System.out.println(maxPowerOf2(17));        //16
     }
 
     public static int maxPowerOf2(int x) {
         x |= x >> 1;
+        System.out.println(x);
         x |= x >> 2;
+        System.out.println(x);
+
         x |= x >> 4;
+        System.out.println(x);
+
         x |= x >> 8;
+        System.out.println(x);
+
         x |= x >> 16;
-        return x - (x >> 1);
+        // |= x >> 8;
+
+        return x ^ (x >> 1);
     }
 }
